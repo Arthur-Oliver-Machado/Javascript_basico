d0b6fe090ef7aa679b6e5921aa583c5b971f2079
Arthur Oliver <arthurolivermachado@gmail.com>
2026-05-22 16:36:52 -0300

    Update script.js

--- Aula 03/exercicios/js/script.js
+++ Aula 03/exercicios/js/script.js
@@ -1,7 +1,13 @@
+//Variaveis
+
 let n1 = 8;
 let n2 = 4;
 let n3 = 12;
 let media = (n1 + n2 + n3)/3;
+let n4 = 67;
+let resto = n4 % 2;
+let resultado = resto === 0;
 
-
+//Prints
 console.log(media);
+console.log(resultado);
