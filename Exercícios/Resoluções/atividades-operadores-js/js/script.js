cd1cad70b859fc1d1ed5b95dc0e67a33980a4272
Arthur Oliver <arthurolivermachado@gmail.com>
2026-06-02 15:51:29 -0300

    Resolução de exercícios de 1 a 6

---
+++ Exercícios/Resoluções/atividades-operadores-js/js/script.js
@@ -0,0 +1,33 @@
+//Operadores Aritméticos
+
+let n1 = 30;
+let n2 = 37;
+let numero = 7;
+let resto = numero % 2;
+let resultado = resto === 0;
+let resultado2 = 2 ** 2;
+let resultado3 = 2 ** 3;
+let celsius = 3;
+let fahrenheit = (celsius * 9/5) + 32;
+let nota1 = 7.5;
+let nota2 = 9.8;
+let nota3 = 6.7;
+let soma = nota1 + nota2 + nota3;
+let media = soma/3;
+let valorPago = 90
+let precoProduto = 67
+let troco = valorPago - precoProduto
+
+
+console.log (n1 + n2);
+console.log (n1/n2);
+console.log (n1*n2);
+console.log (resultado);
+console.log (resultado2)
+console.log (resultado3)
+console.log (fahrenheit)
+console.log (media)
+console.log (troco)
+///////////////
+
+
