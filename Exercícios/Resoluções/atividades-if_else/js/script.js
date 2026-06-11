19ac819a28452410c33123e9af7f37c51e68ead4
Arthur Oliver <arthurolivermachado@gmail.com>
2026-06-11 13:49:32 -0300

    Update script.js

---
+++ Exercícios/Resoluções/atividades-if_else/js/script.js
@@ -0,0 +1,11 @@
+let nums1 = parseFloat (prompt("Digite um número"));
+let nums2 = parseFloat (prompt("Digite um número"));
+
+if (nums1 > nums2) {
+    alert(`O Maior número é ${nums1}`);
+} else if (nums2 > nums1) {
+    alert(`O Maior número é ${nums2}`); 
+} else {
+    alert('Ambos tem mesmo valor'); 
+}
+
