f6989ecbc8499cfb6af85c7526308b93fa46ed59
Arthur Oliver <arthurolivermachado@gmail.com>
2026-06-12 13:36:11 -0300

    Update script.js

--- Exercícios/Resoluções/atividades-if_else/js/script.js
+++ Exercícios/Resoluções/atividades-if_else/js/script.js
@@ -9,3 +9,37 @@
 //     alert('Ambos tem mesmo valor'); 
 // }
 
+// let calc = parseFloat(prompt("Digite o valor de X na equação a**2+bx+c=42 sendo que a=1; b=-10; c=67"));
+
+
+// if (calc ===5) {
+//     alert("O Valor esta correto");
+// } else if (calc >0) {
+//     alert("Valor incorreto, porém positivo");
+// } else if (calc <40) {
+//     alert("Valor incorreto e negativo");
+// }
+
+// let sexo = String(prompt("Digite seu sexo."));
+
+// switch (sexo.toUpperCase()) {
+//     case "F":
+//     case "FEMININO":
+//         alert("F --> Feminino");
+//         break;
+//     case "M":
+//     case "MASCULINO":
+//         alert("M --> Masculino")
+//         break;
+//     default:
+//         alert("Invalido")
+// }
+
+let letra = prompt("Digite uma letra:").toLowerCase();
+
+
+if (letra === "A" || letra === "E" || letra === "I" || letra === "O" || letra === "U") {
+    console.log("É uma VOGAL");
+} else {
+    console.log("É uma CONSOANTE");
+}
